f8e158acfcc8187f7675f2f43797dcd619c29467
JoshuU1610 <hernandezmorenojosemiguel@gmail.com>
2023-09-26 15:39:09 -0400

    Se hizo el filtrado

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const inputSearch = document.getElementById('Input-Search')
+const myUL = document.querySelector('.cards');
+
 function countrysinf (done) {
     const results = fetch("https://restcountries.com/v3.1/all");
     results
@@ -11,15 +14,15 @@ countrysinf(data => {
     console.log(data);
     data.forEach(country => {
         
-        const myUL = document.querySelector('.cards')
+        
         const li = document.createElement('li');
-        console.log(country);
+        console.log(country.code);
         console.log('languages' in country);
         let languages = ' ';
         if('languages' in country){
             languages = Object.values(country?.languages).toLocaleString();
         } else {
-            languages = 'No tiene';
+            languages = 'Dont Have';
         }
         
 
@@ -27,14 +30,48 @@ countrysinf(data => {
         li.innerHTML = `<img src="${country.flags.svg}" alt="flag">
         <h1>${country.name.common}</h1>
         <h2>${country.name.official}</h2>
-        <p>${languages}</p>
-        <p>${country.capital}</p>
-        <p>${country.flag}</p>`;
+        <p><span>Languages:</span> ${languages}</p>
+        <p><span>Capital:</span> ${country.capital}</p>
+        <p><span>Code:</span> ${country.flag}</p>`;
 
 
         li.classList.add('card');
+        li.classList.add('hide');
         myUL.append(li);
     });
 
+    
+})
+
+inputSearch.addEventListener('keyup', e => {
+    const valorFiltro = inputSearch.value.toLowerCase();
+    const lista = document.querySelector(".cards"); 
+    const elementos = lista.getElementsByTagName("li");
+    const regex = /^\s*$/;
+
+    for (let i = 0; i < elementos.length; i++) {
+        const h1 = elementos[i].getElementsByTagName("h1")[0];
+        const textoH1 = h1.textContent.toLowerCase();
+        let coincide = true;
+    
+        for (let j = 0; j < valorFiltro.length; j++) {
+          if (textoH1[j] !== valorFiltro[j]) {
+            coincide = false;
+            break;
+          }
+        }
+    
+        if (coincide) {
+          elementos[i].style.display = "block";
+        } else {
+          elementos[i].style.display = "none";
+        }
+      }
+
+    if(inputSearch.value === '' || regex.test(inputSearch)){
+        for (let i = 0; i < elementos.length; i++){
+            elementos[i].style.display = "none";
+        }
+    }
     
 })
